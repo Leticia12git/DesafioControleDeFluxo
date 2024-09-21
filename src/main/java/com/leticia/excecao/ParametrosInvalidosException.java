30ab2a92a4e30ae90270014bf9311d2bd9ce8090
Leticia Lima <leticia_lele.lima@hotmail.com>
2024-09-20 21:10:12 -0300

    subindo o projeto da DIO no github

---
+++ src/main/java/com/leticia/excecao/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package com.leticia.excecao;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String message) {
+       super(message);
+    }
+}
